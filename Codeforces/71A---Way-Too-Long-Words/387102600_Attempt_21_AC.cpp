670e3973f83f08e34370dbd7feae99ff94742134
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 09:52:49 +0530

    [Codeforces] 71A---Way-Too-Long-Words - Accepted (ID: 387102600)

---
+++ Codeforces/71A---Way-Too-Long-Words/387102600_Attempt_21_AC.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (cin >> n) {
+        while (n--) {
+            string s;
+            cin >> s;
+            if (s.length() > 10) {
+                cout << s.front() << (s.length() - 2) << s.back() << "\n";
+            } else {
+                cout << s << "\n";
+            }
+        }
+    }
+    return 0;
+}

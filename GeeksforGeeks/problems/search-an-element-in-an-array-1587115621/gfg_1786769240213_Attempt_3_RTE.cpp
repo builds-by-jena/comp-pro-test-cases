6bd57e3a53d3a97c1039576a99186a0fd5ca9730
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 10:17:27 +0530

    [GeeksforGeeks] Array-Search - Runtime Error (ID: gfg_1786769240213)

---
+++ GeeksforGeeks/problems/search-an-element-in-an-array-1587115621/gfg_1786769240213_Attempt_3_RTE.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    int search(vector<int>& arr, int x) {
+        // Trigger runtime out-of-bounds error
+        vector<int> emptyVec;
+        return emptyVec.at(999);
+    }
+};

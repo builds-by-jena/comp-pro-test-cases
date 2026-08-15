d4658f46f26e38b2e78a9d56d81d1218dab06636
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 09:53:41 +0530

    [Codeforces] 71A---Way-Too-Long-Words - Compilation error (ID: 387102649)

---
+++ Codeforces/71A---Way-Too-Long-Words/387102649_Attempt_22_CE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Missing semicolon and undeclared identifier to trigger CE
+    undefined_syntax_error
+    int n
+    cin >> n;
+    return 0;
+}

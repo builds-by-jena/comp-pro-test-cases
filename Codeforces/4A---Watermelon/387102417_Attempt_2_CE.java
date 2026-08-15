abebd9206991b4b62a0da45d02480e440f568b2a
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 09:50:00 +0530

    [Codeforces] 4A---Watermelon - Compilation error (ID: 387102417)

---
+++ Codeforces/4A---Watermelon/387102417_Attempt_2_CE.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Intentional syntax errors: undefined variable and missing semicolon
+        this_is_a_compilation_error
+        int w = sc.nextInt()
+        
+        System.out.println("NO");
+    }
+}

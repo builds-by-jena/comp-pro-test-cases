56409ffcffa6220b8fa55faae86b84824118464f
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 10:23:43 +0530

    [CodeChef] Number-Mirror - Time Limit Exceeded (ID: cc_1786769620838)

---
+++ CodeChef/problems/START01/cc_1786769620838_Attempt_3_TLE.sh
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            System.out.println(n);
+        }
+        sc.close();
+    }
+}sdf

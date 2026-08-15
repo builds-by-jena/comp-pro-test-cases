17486e09ebbbca4780bf3edaafe84fb2ded5662e
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 09:57:37 +0530

    [LeetCode] 1-Two-Sum - Runtime Error (ID: 2107202140)

---
+++ LeetCode/1-Two-Sum/2107202140_Attempt_3_RTE.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // Deliberate out-of-range exception
+        std::vector<int> emptyVec;
+        return {emptyVec.at(100)}; 
+    }
+};

f0f6e59aa04efc0a8bc2551824e370ea21d278b1
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 10:22:43 +0530

    Delete CodeChef/problems directory

--- CodeChef/problems/search-an-element-in-an-array-1587115621/gfg_1786769240213_Attempt_1_TLE.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-  public:
-    int search(vector<int>& arr, int x) {
-        // Trigger runtime out-of-bounds error
-        vector<int> emptyVec;
-        return emptyVec.at(999);
-    }
-};

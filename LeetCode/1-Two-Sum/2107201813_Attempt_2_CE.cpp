968c3823bcc164bcb391c386b8630669f4787f5f
builds-by-jena <learncodingfast01@gmail.com>
2026-08-15 09:57:05 +0530

    [LeetCode] 1-Two-Sum - Compile Error (ID: 2107201813)

---
+++ LeetCode/1-Two-Sum/2107201813_Attempt_2_CE.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // Syntax error & missing return type/semicolons
+        undeclared_broken_code
+        return 0
+    }
+};
